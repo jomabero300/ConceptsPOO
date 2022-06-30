7bc216b64e82b02656cdde81704ffe1eac848a52
jomabero300 <jomabero300@yahoo.com>
2022-06-29 23:23:55 -0500

    Finish Date Class

---
+++ TSConceptsPOO/DayException.cs
@@ -0,0 +1,25 @@
+using System.Runtime.Serialization;
+
+namespace TSConceptsPOO
+{
+    [Serializable]
+    public class DayException : Exception
+    {
+        public DayException()
+        {
+            
+        }
+        public DayException(string? message) : base(message)
+        {
+        }
+
+        public DayException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+
+        protected DayException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }        
+
+    }
+}
---
+++ TSConceptsPOO/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using TSConceptsPOO;
+Console.WriteLine("POO Concepts");
+Console.WriteLine("============");
+try
+{
+    Console.WriteLine(new Date(2024,2,29));
+    Console.WriteLine(new Date(2022,6,9));
+    Console.WriteLine(new Date(2022,11,15));    
+}
+catch (System.Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
